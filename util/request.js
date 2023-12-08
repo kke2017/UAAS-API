2200b94e6fa0fe786f364d50e47f7a225f964190
kke2017 <72059419+kke2017@users.noreply.github.com>
2023-12-08 21:47:53 +0800

    Update request.js

--- util/request.js
+++ util/request.js
@@ -1,9 +1,9 @@
-const axios = require("axios")
-const iconv = require("iconv-lite")
-const { getUserAgent } = require("./util")
-const getSchoolConfig = require("../util/schoolConfig")
+const axios = require( "axios" )
+const iconv = require( "iconv-lite" )
+const { getUserAgent } = require( "./util" )
+const getSchoolConfig = require( "../util/schoolConfig" )
 const schoolConfig = getSchoolConfig()
-const responseInterceptors = require("./responseInterceptors")
+const responseInterceptors = require( "./responseInterceptors" )
 
 const defaultHeaders = {
   Host: schoolConfig.host,
@@ -20,65 +20,64 @@ const responseType =
     ? "json"
     : "arraybuffer"
 
-const createRequest = (axiosOptions, options = {}) => {
-  return new Promise((resolve, reject) => {
-    const axiosInstance = axios.create({
-      baseURL: `${schoolConfig.ssl === false ? "http" : "https"}://${
-        schoolConfig.domain
-      }`,
+const createRequest = ( axiosOptions, options = {} ) => {
+  return new Promise( ( resolve, reject ) => {
+    const axiosInstance = axios.create( {
+      baseURL: `${ schoolConfig.ssl === false ? "http" : "https" }://${ schoolConfig.domain
+        }`,
       timeout: 20000,
       responseType,
       maxRedirects: 0, // 不重定向
-    })
+    } )
     // set default headers
     axiosOptions.headers = { ...axiosOptions.headers, ...defaultHeaders }
     // set UserAgent
-    if (!axiosOptions.headers["User-Agent"]) {
-      axiosOptions.headers["User-Agent"] = getUserAgent()
+    if ( !axiosOptions.headers[ "User-Agent" ] ) {
+      axiosOptions.headers[ "User-Agent" ] = getUserAgent()
     }
-    if (axiosOptions.method?.toUpperCase() == "POST") {
-      axiosOptions.headers["Content-Type"] = "application/x-www-form-urlencoded"
+    if ( axiosOptions.method?.toUpperCase() == "POST" ) {
+      axiosOptions.headers[ "Content-Type" ] = "application/x-www-form-urlencoded"
     }
-    console.log("axiosOptions", axiosOptions)
+    console.log( "axiosOptions", axiosOptions )
     let returnDataType = "data"
-    if (options.returnDataType) {
+    if ( options.returnDataType ) {
       returnDataType = options.returnDataType
     }
     // 响应拦截器
     axiosInstance.interceptors.response.use(
-      (res) => {
+      ( res ) => {
         // 状态码为2xx时，拦截
         // 接收arraybuffer需要decode
-        if (responseType === "arraybuffer") {
-          res.data = iconv.decode(res.data, schoolConfig.chatset || "utf-8")
+        if ( responseType === "arraybuffer" ) {
+          res.data = iconv.decode( res.data, schoolConfig.chatset || "utf-8" )
         }
-        const diyInterceptor = responseInterceptors[process.env.SCHOOL_CODE]
-        if (diyInterceptor && options.notInterceptor !== true) {
-          return diyInterceptor(res)
+        const diyInterceptor = responseInterceptors[ process.env.SCHOOL_CODE ]
+        if ( diyInterceptor && options.notInterceptor !== true ) {
+          return diyInterceptor( res )
         }
         return res
       },
-      (err) => {
+      ( err ) => {
         // 状态码大于2xx时，拦截
         // 处理302的情况
-        if (options.redirect === true) {
-          return Promise.resolve(err)
+        if ( options.redirect === true ) {
+          return Promise.resolve( err )
         }
-        return Promise.reject(err)
+        return Promise.reject( err )
       }
     )
     axiosInstance
-      .request(axiosOptions)
-      .then((res) => {
-        if (returnDataType == "all") {
-          return resolve(res)
+      .request( axiosOptions )
+      .then( ( res ) => {
+        if ( returnDataType == "all" ) {
+          return resolve( res )
         }
-        return resolve(res[returnDataType])
-      })
-      .catch((err) => {
-        reject(err)
-      })
-  })
+        return resolve( res[ returnDataType ] )
+      } )
+      .catch( ( err ) => {
+        reject( err )
+      } )
+  } )
 }
 
 module.exports = createRequest
